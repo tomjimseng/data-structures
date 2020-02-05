793d13ef5d014c872d75f950c72c87a6e345d655
tomjimseng <tomjimseng@gmail.com>
2020-02-05 14:59:46 -0800

    Update to stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,35 @@
 var Stack = function() {
+  //specification - object
+  //do not use Array, 
   var someInstance = {};
+
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    if (size === 0) {
+      return 0;
+    }
+    
+    size--;
+
+    var deleted = someInstance[size];
+    delete someInstance[size];
+    return deleted;
+    
+
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
