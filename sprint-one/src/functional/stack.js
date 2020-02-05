eae91050d13833342aa70a6cde9d3fb1572c3cbd
tomjimseng <tomjimseng@gmail.com>
2020-02-05 15:07:23 -0800

    Update stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,6 +1,9 @@
 var Stack = function() {
   //specification - object
-  //do not use Array, 
+  //do not use Array, input can be any value; when push occurs increase size by 1; when pop occurs decrease size by 1;
+  // purpose of the function is to show the increase and decrease in size as variables are added or removed
+  //input is any value which is pushed into the object then either removed or left within the object
+
   var someInstance = {};
 
   var size = 0;
@@ -10,15 +13,23 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //set someInstance to be a value in regards to size
+    // increment size
     someInstance[size] = value;
     size++;
   };
 
   someInstance.pop = function() {
+    // edge case if size is already zero return zero not negative
+    //decrement size 
+    // create deleted var to return for pop();
+    // delete Instance[size]
+    // return deleted
+
     if (size === 0) {
       return 0;
     }
-    
+
     size--;
 
     var deleted = someInstance[size];
@@ -29,6 +40,7 @@ var Stack = function() {
   };
 
   someInstance.size = function() {
+    // return size after increment and decrement as someInstance.size
     return size;
   };
 
