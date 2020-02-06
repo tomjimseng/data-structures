555d7180388047dca030ea37916d916e7d78597e
tomjimseng <tomjimseng@gmail.com>
2020-02-05 23:52:57 -0800

    update prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstanceOf = Object.create(queueMethods);
+  someInstanceOf.num = 0;
+  someInstanceOf.back = 0;
+  someInstanceOf.front = 0;
+  return someInstanceOf;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+	this[this.back] = value;
+	this.back++;
+};
 
+queueMethods.dequeue = function() {
+	if (this.back - this.front === 0) {
+		return 0;
+	}
+	
+	var deleted = this[this.front];
+	delete this[this.front];
+	this.front++;
+	return deleted;
+};
+
+queueMethods.size = function() {
+	this.num = this.back - this.front;
+	return this.num;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstanceOf = Object.create(stackMethods);
+  someInstanceOf.num = 0;
+  return someInstanceOf;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+	this[this.num] = value;
+	this.num++;
+};
+
+stackMethods.pop = function() {
+	if (this.num === 0) {
+		return 0;
+	}
+	this.num--;
+	var deleted = this[this.num];
+	delete this[this.num];
+	return deleted;
+};
+
+stackMethods.size = function() {
+	return this.num;
+};
 
 
