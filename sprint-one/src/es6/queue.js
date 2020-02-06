9d53b43b74c6850decc7d6048d28c44f05d7d163
tomjimseng <tomjimseng@gmail.com>
2020-02-06 00:24:00 -0800

    update es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,33 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.num;
+  	this.back = 0;
+  	this.front = 0;
+  	this.container = {};
   }
+
+
+enqueue(value) {
+this.container[this.back] = value;
+this.back++;
+}
+
+
+dequeue() {
+if (this.back - this.front === 0) {
+	return 0;
+}
+var deleted = this.container[this.front];
+delete this.container[this.front];
+this.front++;
+return deleted;
+}
+
+
+size() {
+this.num = this.back - this.front;
+return this.num;
+}
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.num = 0;
+  	this.container = {};
   }
 
+  push(value) {
+  	this.container[this.num] = value;
+  	this.num++;
+  }
+
+  pop() {
+  	if (this.num === 0) {
+  		return 0;
+  	}
+  	this.num--;
+  	var deleted = this.container[this.num];
+  	delete this.container[this.num];
+  	return deleted;
+  }
+
+  size() {
+  	return this.num;
+  }
 }
+
