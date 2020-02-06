60732de0902960c19e2dbc2e9d9d64b1b7dfd68f
tomjimseng <tomjimseng@gmail.com>
2020-02-06 00:05:23 -0800

    update pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.num;
+  this.back = 0;
+  this.front = 0;
+
 };
 
+Queue.prototype.enqueue = function(value) {
+	this[this.back] = value;
+	this.back++;
+};
 
+Queue.prototype.dequeue = function(value) {
+	if(this.back - this.front === 0) {
+		return 0;
+	}
+	var deleted = this[this.front];
+	delete this[this.front];
+	this.front++;
+	return deleted;
+};
+
+Queue.prototype.size = function(value) {
+	this.num = this.back - this.front;
+	return this.num;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.num = 0;
 };
 
+Stack.prototype.push = function(value) {
+	this[this.num] = value;
+	this.num++;
+};
 
+Stack.prototype.pop = function(value) {
+	if(this.num === 0) {
+		return 0;
+	}
+	this.num--;
+	var deleted = this[this.num];
+	delete this[this.num];
+	return deleted;
+};
+
+Stack.prototype.size = function(value) {
+	return this.num;
+};
