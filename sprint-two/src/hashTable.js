6e9cea0f0c4f023bf64886f2150df08b941235b2
tomjimseng <tomjimseng@gmail.com>
2020-02-07 00:19:58 -0800

    Update hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,37 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (bucket) {
+  	for (var i = 0; i < bucket.length; i++) {
+  		if(bucket[i][0] === k) {
+  			bucket[i][1] === v;
+  		} else {
+  			bucket.push([k, v]);
+  		}
+  	}
+
+  } else {
+  	this._storage.set(index, [k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+  	if (bucket[i][0] === k) {
+  		return bucket[i][1];
+  	}
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+  	bucket.splice(i, 1);
+  }
 };
 
 
